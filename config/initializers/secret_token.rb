9fb982a15c3fc19c4e3630eb6e77ea426394ae8b
Ian Smith <ianhenrysmith@gmail.com>
2013-01-04 08:02:35 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ianhenrysmith::Application.config.secret_token = '84a509952a671ca4690c8aac0a3f698166797f8b5c9b3f2cb581010e4c4659b4d049df606c603512437fcb7c8f8b11a9a4e3881a5b3d85333ef988ea45521457'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ianhenrysmith::Application.config.session_store :cookie_store, key: '_ianhenrysmith_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ianhenrysmith::Application.config.session_store :active_record_store
